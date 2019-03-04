cfa37c5307b1ffdb5562faef18533d23e361cc0b
yangmi <33408426+yangmi0829@users.noreply.github.com>
2019-03-04 17:26:01 +0800

    Update Directive.vue

--- src/views/directive/Directive.vue
+++ src/views/directive/Directive.vue
@@ -2,10 +2,18 @@
   <div>
     <p>自定义指令输入框保留两位小数</p>
     <div>
-      原生input:<input type="number" v-model="num" v-native-float/>{{num}}
+      原生input:<input type="number" v-model="num" v-native-float.num/>{{num}}
+      原生input lazy:<input type="number" v-model="lazy_num" v-native-float:lazy.lazy_num/>{{lazy_num}}
     </div>
     <div>
-      element-input:<el-input type="number" v-model="el_num" v-el-float/>{{el_num}}
+      原生input 对象.属性:<input type="number" v-model="obj.num" v-native-float.obj.num/>{{obj.num}}
+      原生input 对象.属性 lazy:<input type="number" v-model="obj.lazy_num" v-native-float:lazy.obj.lazy_num/>{{obj.lazy_num}}
+    </div>
+    <div>
+      element-input:<el-input type="number" v-model="el_num" v-el-float.el_num/>{{el_num}}
+    </div>
+    <div>
+      element-input 对象.属性:<el-input type="number" v-model="obj.el_num" v-el-float.obj.el_num/>{{obj.el_num}}
     </div>
   </div>
 </template>
@@ -15,8 +23,16 @@ export default {
   name: 'directive',
   data () {
     return {
+      obj: {
+        num: '',
+        lazy_num: '',
+        el_num: '',
+        lazy_el_num: ''
+      },
       num: '',
-      el_num: ''
+      el_num: '',
+      lazy_num: '',
+      lazy_el_num: ''
     }
   },
   created () {
