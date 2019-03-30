255fd60b1dd53e96fae05a23395be378deec0bbc
yangmi <33408426+yangmi0829@users.noreply.github.com>
2019-03-30 16:34:15 +0800

    Create base-dialog.vue

---
+++ src/components/dialog/base-dialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-dialog
+    v-bind="$attrs"
+    v-on="$listeners">
+    <template slot="title">
+      <slot name="title"></slot>
+    </template>
+    <slot></slot>
+    <template slot="footer">
+      <slot name="footer"></slot>
+    </template>
+  </el-dialog>
+</template>
+<script>
+export default {
+  name: 'base-dialog',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  created () {},
+  mounted () {},
+  activated () {},
+  methods: {},
+  computed: {},
+  watch: {}
+}
+</script>
+
+<style scoped>
+</style>
